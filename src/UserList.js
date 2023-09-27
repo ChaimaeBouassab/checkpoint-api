583f7b66ab3c24c80cf92fb5595979441463633a
ChaimaeBouassab <shaimaabouassab@gmail.com>
2023-09-27 11:34:49 +0100

    apidone

---
+++ src/UserList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    axios.get(apiUrl)
+      .then((response) => {
+        setListOfUsers(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>List of Users</h2>
+      <ul>
+        {listOfUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
